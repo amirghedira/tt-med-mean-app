c93e11cb64d91b28d5ac052e057340db1de32893
amirghedira <amirghedira06@gmail.com>
2021-05-08 17:19:00 +0100

    adding interfaces

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldControl, MatFormFieldModule, MatInputModule, MatRadioModule, MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginPageComponent,
+        SignupPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatRadioModule,
+        MatFormFieldModule,
+        MatInputModule
+
+    ],
+    providers: [{
+        provide: MAT_RADIO_DEFAULT_OPTIONS,
+        useValue: { color: 'primary' },
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
