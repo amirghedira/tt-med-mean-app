d97c2e799f3a4a2fd82f2eb301aca927d399a11d
sahar195 <saharayed99@gmail.com>
2021-05-25 16:10:02 +0100

    today'sUpdate

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{DatePipe} from'@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +12,7 @@ import { DoctorModule } from './doctor/doctor.module';
 import { AdminModule } from './admin/admin.module';
 import { NurseModule } from './nurse/nurse.module';
 import { AuthModule } from './auth/auth.module';
+import { from } from 'rxjs';
 
 
 @NgModule({
